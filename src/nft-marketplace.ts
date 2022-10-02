c1335c35c33b17acf660a568cbcd92424495cf37
sclongyang@126.com <sclongyang@126.com>
2022-10-02 18:24:22 +0800

    bug fix

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -3,13 +3,12 @@ import {
   NFTMarketplace,
   AddedItem as AddedItemEvent,
   BuyNFT as BuyNFTEvent,
-  DeletedItem as DeletedItemEvent,
-  WithdrawProceeds as WithdrawProceedsEvent
+  DeletedItem as DeletedItemEvent
 } from "../generated/NFTMarketplace/NFTMarketplace"
-import { ActiveItem, AddedItem, DeletedItem, BuyNFT, WithdrawProceeds } from "../generated/schema"
+import { ActiveItem, AddedItem, DeletedItem, BuyNFT } from "../generated/schema"
 
-export function handleAddedItem(event: AddedItem): void {
-  let id = getIdFromEventParams(event.tokenId, event.nftAddress)
+export function handleAddedItem(event: AddedItemEvent): void {
+  let id = getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
   let addedItem = AddedItem.load(id)
   let activeItem = ActiveItem.load(id)
   if (!addedItem) {
@@ -18,57 +17,57 @@ export function handleAddedItem(event: AddedItem): void {
   if (!activeItem) {
     activeItem = new ActiveItem(id)
   }
-  addedItem.nftAddress = event.nftAddress
-  addedItem.tokenId = event.tokenId
-  addedItem.price = event.price
-  addedItem.seller = event.seller
+  addedItem.nftAddress = event.params.nftAddress
+  addedItem.tokenId = event.params.tokenId
+  addedItem.price = event.params.price
+  addedItem.seller = event.params.seller
 
-  activeItem.nftAddress = event.nftAddress
-  activeItem.tokenId = event.tokenId
-  activeItem.price = event.price
-  activeItem.seller = event.seller
+  activeItem.nftAddress = event.params.nftAddress
+  activeItem.tokenId = event.params.tokenId
+  activeItem.price = event.params.price
+  activeItem.seller = event.params.seller
   activeItem.buyer = Address.fromString("0x0000000000000000000000000000000000000000")
 
   addedItem.save()
   activeItem.save()
 }
 
-export function handleBuyNFT(event: BuyNFT): void {
+export function handleBuyNFT(event: BuyNFTEvent): void {
   let itemBought = BuyNFT.load(
-    getIdFromEventParams(event.tokenId, event.nftAddress)
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
   )
   let activeItem = ActiveItem.load(
-    getIdFromEventParams(event.tokenId, event.nftAddress)
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
   )
   if (!itemBought) {
     itemBought = new BuyNFT(
-      getIdFromEventParams(event.tokenId, event.nftAddress)
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
     )
   }
-  itemBought.buyer = event.buyer
-  itemBought.nftAddress = event.nftAddress
-  itemBought.tokenId = event.tokenId
-  activeItem!.buyer = event.buyer
+  itemBought.buyer = event.params.buyer
+  itemBought.nftAddress = event.params.nftAddress
+  itemBought.tokenId = event.params.tokenId
+  activeItem!.buyer = event.params.buyer
 
   itemBought.save()
   activeItem!.save()
 }
 
-export function handleDeletedItem(event: DeletedItem): void {
+export function handleDeletedItem(event: DeletedItemEvent): void {
   let itemCanceled = DeletedItem.load(
-    getIdFromEventParams(event.tokenId, event.nftAddress)
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
   )
   let activeItem = ActiveItem.load(
-    getIdFromEventParams(event.tokenId, event.nftAddress)
+    getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
   )
   if (!itemCanceled) {
     itemCanceled = new DeletedItem(
-      getIdFromEventParams(event.tokenId, event.nftAddress)
+      getIdFromEventParams(event.params.tokenId, event.params.nftAddress)
     )
   }
-  itemCanceled.seller = event.seller
-  itemCanceled.nftAddress = event.nftAddress
-  itemCanceled.tokenId = event.tokenId
+  itemCanceled.seller = event.params.seller
+  itemCanceled.nftAddress = event.params.nftAddress
+  itemCanceled.tokenId = event.params.tokenId
   activeItem!.buyer = Address.fromString("0x000000000000000000000000000000000000dEaD")
 
   itemCanceled.save()
